38a6a8a1b2925b0249adb90c1aca3d4506205c07
ggg21on <gomesgalikosky@gmail.com>
2024-08-09 20:43:54 -0300

    Tutorial 5 - Chamadas de API Com Axios

--- real-world-vue/src/App.vue
+++ real-world-vue/src/App.vue
@@ -8,6 +8,25 @@
         </nav>
       </div>
     </header>
-    <RouterView />
+    <main>
+      <RouterView />
+    </main>
   </div>
 </template>
+
+<style scoped>
+#layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+nav {
+  text-align: center;
+}
+
+main {
+  flex: 1;
+  padding: 1rem;
+}
+</style>
